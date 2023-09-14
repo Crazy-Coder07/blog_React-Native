15d6e59d290198f9e6dcc9d2eda2ed874d6b574f
Aditya Ranjan <108622072+Crazy-Coder07@users.noreply.github.com>
2023-09-14 18:48:57 +0530

    added server folder

---
+++ server/server.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const server = () => {
+  return (
+    <View>
+      <Text>server</Text>
+    </View>
+  )
+}
+
+export default server
