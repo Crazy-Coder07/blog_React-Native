3b24f13377f0c220142cc7377c6132a3f98a8206
Aditya Ranjan <108622072+Crazy-Coder07@users.noreply.github.com>
2023-09-14 18:39:49 +0530

    created basic Register and login Pages

---
+++ client/App.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Register from './Pages/auth/Register'
+
+const App = () => {
+  return (
+    <Register/>
+  )
+}
+
+export default App
